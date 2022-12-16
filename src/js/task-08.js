bf18a4e873d0bd1652ed51619583dad60e04f95f
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2022-12-16 07:50:56 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+const handleFormElSubmit = Event => {
+  Event.preventDefault();
+  const formElements = Event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Fill in all the fields, please.');
+  }
+
+  console.log({ email, password });
+
+  Event.currentTarget.reset();
+};
+
+formEl.addEventListener('submit', handleFormElSubmit);
