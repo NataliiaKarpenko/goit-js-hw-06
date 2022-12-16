4533340fbd476968bbb606893fecea988d5457e7
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2022-12-16 14:41:56 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const changeColorBtbEl = document.querySelector('.change-color');
+const colorSpanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+const handleChangeColorBtbClick = event => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorSpanEl.textContent = bodyEl.style.backgroundColor;
+};
+
+changeColorBtbEl.addEventListener('click', handleChangeColorBtbClick);
