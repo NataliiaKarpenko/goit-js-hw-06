6bec946b7d928beff8f50bb770e0116acebcb6bc
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2022-12-16 09:10:40 +0200

    updated task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -10,6 +10,12 @@ const handleFormElSubmit = Event => {
     alert('Fill in all the fields, please.');
   }
 
+  // const formData = new FormData(Event.currentTarget);
+  // formData.forEach((value, key) => {
+  //   console.log('name', key);
+  //   console.log('value', value);
+  // });
+
   console.log({ email, password });
 
   Event.currentTarget.reset();
