87a9a352ad2e22b89a49d44242514de1b7ed6314
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2022-12-17 09:37:53 +0200

    updated task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,8 +3,10 @@ const validationInputEl = document.querySelector('#validation-input');
 const handleValidationInputBlur = () => {
   if (validationInputEl.value.length !== Number(validationInputEl.dataset.length)) {
     validationInputEl.classList.add('invalid');
+    validationInputEl.classList.remove('valid');
   } else {
     validationInputEl.classList.add('valid');
+    validationInputEl.classList.remove('invalid');
   }
 };
 validationInputEl.addEventListener('blur', handleValidationInputBlur);
